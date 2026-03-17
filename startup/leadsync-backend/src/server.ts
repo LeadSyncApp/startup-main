cfd9b912c7e01b3b8b6e9fca89c282827cbb296c
leadsyncnew <leadsyncnew@gmail.com>
2026-03-17 11:54:21 +0530

    changed for crashing

--- startup/leadsync-backend/src/server.ts
+++ startup/leadsync-backend/src/server.ts
@@ -1,4 +1,10 @@
 import "dotenv/config";
+// Capture unhandled promise rejections early so the server doesn't crash silently
+process.on("unhandledRejection", (reason, promise) => {
+  // eslint-disable-next-line no-console
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+});
+
 import http from "http";
 import app from "./app";
 import { initSocket } from "./lib/socket";
